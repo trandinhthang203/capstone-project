c0c46091c2bb08c15c2d06f0d849efb9bdd48884
trandinhthang203 <tranthangkhuong203@gmail.com>
2026-04-16 06:54:35 +0700

    fix: increased latency ~ 30s, fields classification

--- app/agents/supervisor/supervisor_agent.py
+++ app/agents/supervisor/supervisor_agent.py
@@ -1,13 +1,11 @@
-from app.agents.base.state import AgentState, FormsOutput, LocationOutput, QAOutput
+from app.agents.base.state import AgentState
 from langgraph.types import Command
 from typing import Literal
-from app.agents.base.utils import get_next_agent
 from app.helpers.utils.common import get_response_llm
 from app.core.config import *
 import json
-from app.db.session import get_db
-from app.agents.supervisor.supervisor_tools import resolve_procedures_fts
 from app.helpers.utils.logger import logging
+from app.helpers.utils.common import read_json
 
 def supervisor_node(state: AgentState) -> Command[Literal["qa"]]:
     user_input = state['user_input']
@@ -17,37 +15,42 @@ def supervisor_node(state: AgentState) -> Command[Literal["qa"]]:
     response = get_response_llm(prompt, user_input)
     data = json.loads(response)
 
-    resolved = []
+    # resolved = []
     procedures = data.get("procedures", [])
     logging.info(f"[supervisor_node] procedures original: {procedures}")
 
-    if procedures:
-        db = next(get_db())
-        try:
-            resolved = resolve_procedures_fts(
-                db=db,
-                user_query=user_input,
-                supervisor_candidates=procedures,
-            )
-        finally:
-            db.close()
+    name_ids = read_json("app/agents/supervisor", "name_id.json")
 
-        if resolved:
-            data["procedures"] = [item["ten_thu_tuc"] for item in resolved]
+    procedure_ids = [name_ids[proc] for proc in procedures if proc in name_ids]    
+    # if procedures:
+    #     db = next(get_db())
+    #     try:
+    #         resolved = resolve_procedures_fts(
+    #             db=db,
+    #             user_query=user_input,
+    #             supervisor_candidates=procedures,
+    #         )
+    #     finally:
+    #         db.close()
 
-    logging.info(f"[supervisor_node]: {resolved}")
+    #     if resolved:
+    #         data["procedures"] = [item["ten_thu_tuc"] for item in resolved]
+
+    # logging.info(f"[supervisor_node]: {resolved}")
 
     return Command(
         goto=data["pipeline"][0],
         update={
-            "procedures": data.get("procedures", []),
-            "resolved_procedures": resolved,
+            "procedures": procedure_ids,
+            # "resolved_procedures": resolved,
             "pipeline": data.get("pipeline", ["qa"]),
             "fields": data.get("fields", [])
         },
     )
 
 
+
+###################################### WHY  INCREASE LATENCY ~ 30S ALTHOUGH ONLY HAVE 2 REQUEST ###########################
 
 
     
