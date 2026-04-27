154e11652a2d75d64317e3a34566e8e06eede867
trandinhthang203 <tranthangkhuong203@gmail.com>
2026-04-28 02:23:39 +0700

    feat: crud session, add endpoint sesssion and more...

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api import api_auth, api_healthcheck, api_user
+from app.api import api_auth, api_healthcheck, api_user, api_session
 
 router = APIRouter()
 
 router.include_router(api_healthcheck.router, tags=["health-check"], prefix="/healthcheck")
 router.include_router(api_auth.router, prefix="/auth", tags=["auth"])
 router.include_router(api_user.router, tags=["user"], prefix="/users")
+router.include_router(api_session.router, tags=["session"], prefix="/sessions")
